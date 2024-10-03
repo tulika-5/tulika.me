197e4a9cc18bcb494dd1d466bee18de12c4eaafa
Tulika <mstulika0512@gmail.com>
2024-10-04 04:12:58 +0530

    updation

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "mstulika0512@gmail.com"; 
+    $subject = "Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again later.";
+    }
+}
+?>
